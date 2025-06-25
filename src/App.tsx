fd1545fc0db297fc79acb4cbeafa79492b5de3d0
Kota Hashihama <kotahashihama@gmail.com>
2025-06-25 22:30:15 +0900

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -188,10 +188,11 @@ function App() {
   // tree.glbを東京タワー付近に1本だけ表示
   const treeModelLayer = new ScenegraphLayer({
     id: 'tree-glb-single',
-    data: [{ position: [139.7454, 35.6586, 33] }], // 東京タワー付近、地面から10m上
+    data: [{ position: [139.7450223163875, 35.65895642091729, 6] }],
     scenegraph: '/tree.glb',
     getPosition: (d: { position: [number, number, number] }) => d.position,
-    sizeScale: 10,
+    getOrientation: () => [0, 0, 90],
+    sizeScale: 5,
     _lighting: 'pbr',
     pickable: true,
   });
